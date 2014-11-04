6ad78d624d4e638cdecda51f57ed4898f48d26c1
blazebot7000 <cgraf@uvm.edu>
2014-11-04 16:37:20 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include ("top.php");
+include ("header.php");
+include ("nav.php");
+?>
+
+
+<h1>WELCOME!</h1>
+
+<p>This site is designed to test your knowledge of computer tips and tricks by providing you with an abundance of missions
+    that will force you to use all of your current knowledge and more to complete.</p>
+
+<?php
+include ("footer.php");
+?>
+</html>
