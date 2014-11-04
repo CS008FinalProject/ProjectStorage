bd81ec5e3a2d9732b657bd5da11bfce93a499ab5
blazebot7000 <cgraf@uvm.edu>
2014-11-04 16:39:33 -0500

    Create mission5.php

---
+++ mission5.php
@@ -0,0 +1,43 @@
+<?php
+include ("top.php");
+include ("header.php");
+include ("nav.php");
+if (isset($_POST["btnSubmit"]) AND empty($errorMsg)) {
+    print "<h1>Wrong pass-key. Try again.</h1>";
+}
+?>
+     <h2>Mission 5</h2>
+        <p>
+            Description of Mission 5
+        </p>
+<article class ="mission">
+    <form action="<?php print $phpSelf; ?>"
+          method="post"
+          id="frmRegister">
+
+        <fieldset class="wrapper">
+            <legend></legend>
+            <fieldset class="contact">
+                <legend>Enter the pass-key to advance to the next mission.</legend>
+                <label for="txtPasskey" class="required">Pass-key
+                    <input type="text" id="txtPasskey" name="txtPasskey"
+                           value="<?php print $uName; ?>"
+                           tabindex="200" maxlength="50" placeholder="Enter the pass-key"
+                           <?php if ($uNameERROR) print 'class="mistake"'; ?>
+                           onfocus="this.select()">
+                </label>
+            </fieldset> <!-- ends contact -->
+
+            <fieldset class="buttons">
+                <legend></legend>
+                <input type="submit" id="btnSubmit" name="btnSubmit" value="Submit" tabindex="900" class="button">
+            </fieldset> <!-- ends buttons -->
+
+        </fieldset> <!-- ends wrapper -->
+    </form>
+
+</article>
+        <?php
+            include ("footer.php");
+        ?>
+</html>
